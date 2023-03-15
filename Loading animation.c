c72e57a51d1bced95a28d2ba2e6495ea05c99d80
VaishakRajesh <127651941+VaishakRajesh@users.noreply.github.com>
2023-03-15 20:40:22 +0530

    Create Loading animation.c

---
+++ Loading animation.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i;
+	for (i = 0; i < 5; i++)
+	{
+		puts("Loading.");
+		sleep(1);
+		clrscr();
+		puts("Loading..");
+		sleep(1);
+		clrscr();
+		puts("Loading...");
+		sleep(1);
+		clrscr();
+		puts("Loading....");
+		sleep(1);
+		clrscr();
+		puts("Loading.....");
+		sleep(1);
+		clrscr();
+	}
+}
